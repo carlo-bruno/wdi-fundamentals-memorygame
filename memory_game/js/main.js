7630619b5d031bbe3f3229b3b3c892381bb15908
Carlo Bruno <carlofbruno@gmail.com>
2018-12-20 08:12:23 -0800

    updated game logic with functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,38 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
+// var cardOne = cards[0];
+// cardsInPlay.push(cardOne);
 
-console.log(`User flipped ${cardOne}`);
+// console.log(`User flipped ${cardOne}`);
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
+// var cardTwo = cards[2];
+// cardsInPlay.push(cardTwo);
 
-console.log(`User flipped ${cardTwo}`);
+// console.log(`User flipped ${cardTwo}`);
 
-if (cardsInPlay.length === 2) {
+// if (cardsInPlay.length === 2) {
+//   if (cardsInPlay[0] === cardsInPlay[1]) {
+//     alert("You found a match!");
+//   } else {
+//     alert("Sorry, try again.");
+//   }
+// }
+
+function flipCard(cardId) {
+  console.log(`User flipped ${cards[cardId]}`);
+  cardsInPlay.push(cards[cardId]);
+}
+
+function checkMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
+    console.log("You found a match!");
   } else {
-    alert("Sorry, try again.");
+    console.log("Sorry, try again.");
   }
 }
+
+flipCard(0);
+flipCard(2);
+console.log(cardsInPlay);
+checkMatch();
